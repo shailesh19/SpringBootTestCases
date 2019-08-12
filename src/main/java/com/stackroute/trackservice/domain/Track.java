00c5f5d342d7a52a900d29a18d27fb0628bbad00
shailesh19 <beingshailesh19@gmail.com>
2019-08-12 23:55:28 +0530

    Modified Code.

--- src/main/java/com/stackroute/trackservice/domain/Track.java
+++ src/main/java/com/stackroute/trackservice/domain/Track.java
@@ -6,26 +6,17 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
 
-/**
- * Making track as entity to store tracks details
- * like trackId, trackName, comments into table of DB.
- */
-//@Entity
-    @Document(collection = "tracks")
-//generates no-arg constructor
+@Document(collection = "tracks")
+//Generates no-arg constructor
 @NoArgsConstructor
-//generates all-args constructor with all properties in the class
+//Generates all-args constructor with all properties in the class
 @AllArgsConstructor
-//generates Getter,Setter Methods,toString() and hashcode()
+//Generates Getter,Setter Methods,toString() and hashcode()
 @Data
 public class Track {
     @Id
     private int trackId;
     private String trackName;
     private String comments;
-
-
 }
