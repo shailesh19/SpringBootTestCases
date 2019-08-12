50f0cd7fba8e30e04bd394836fbc5409c53cc8aa
shailesh19 <beingshailesh19@gmail.com>
2019-08-12 22:56:35 +0530

    Modified Method Names for TrackRepository and modified code for UpdateTrack.

--- src/test/java/com/stackroute/trackservice/repostiory/TrackRepositoryTest.java
+++ src/test/java/com/stackroute/trackservice/repostiory/TrackRepositoryTest.java
@@ -21,91 +21,79 @@ import java.util.Optional;
 @DataMongoTest
 public class TrackRepositoryTest {
 
-        @Autowired
-        private TrackRepository trackRepository;
-        private Track track;
-        private Track trackToUpdate;
+    @Autowired
+    private TrackRepository trackRepository;
+    private Track track;
+    private Track trackToUpdate;
 
-        @Before
-        public void setUp()
-        {
-            track = new Track();
-            track.setTrackId(10);
-            track.setTrackName("John");
-            track.setComments("test1");
-            trackToUpdate=new Track();
-            trackToUpdate.setTrackId(10);
-            trackToUpdate.setTrackName("Johnson");
-            trackToUpdate.setComments("testupdate");
+    @Before
+    public void setUp() {
+        track = new Track();
+        track.setTrackId(10);
+        track.setTrackName("John");
+        track.setComments("test1");
+        trackToUpdate = new Track();
+        trackToUpdate.setTrackId(10);
+        trackToUpdate.setTrackName("Johnson");
+        trackToUpdate.setComments("testupdate");
+    }
 
+    @After
+    public void tearDown() {
+        track = null;
+        trackToUpdate = null;
+        trackRepository.deleteAll();
+    }
 
-        }
-
-        @After
-        public void tearDown(){
-            track=null;
-            trackToUpdate=null;
-            trackRepository.deleteAll();
-        }
-
-//Test for savetrack
-        @Test
-        public void givenTrackIdShouldFetchTheRespectiveTrack(){
-            trackRepository.save(track);
-            Track fetchTrack = trackRepository.findById(track.getTrackId()).get();
-            Assert.assertEquals(10,fetchTrack.getTrackId());
-
-        }
-
-//        Test for saveTrackFailure
-        @Test
-        public void GivenTrackIdShouldShowSaveTrackFailure(){
-            Track testTrack = new Track(10,"anil","comment");
-            trackRepository.save(testTrack);
-            Track fetchTrack= trackRepository.findById(track.getTrackId()).get();
-            Assert.assertNotSame(testTrack,fetchTrack);
-        }
-
-//        Test for getAllTtracks
-        @Test
-        public void testShouldReturnAllTracks() {
-            Track track1 = new Track(12,"vadi pulla","comments for vadi pulla");
-            Track track2 = new Track(13,"kanamma","comments for kanamma");
-            trackRepository.save(track1);
-            trackRepository.save(track2);
-            List<Track> testTracks= new ArrayList<>();
-            testTracks.add(track1);
-            testTracks.add(track2);
-
-            List<Track> outputList = trackRepository.findAll();
-            Assert.assertEquals(testTracks,outputList);
-        }
-
-//        Test for deleteTrack
-        @Test
-        public void GivenTrackIdShouldDeletetrackAndReturnNull(){
-            trackRepository.save(track);
-            trackRepository.delete(track);
-            Optional<Track> expected=Optional.empty();
-
-            Assert.assertEquals(expected,trackRepository.findById(track.getTrackId()));
-        }
-
-//        Test for updatingTrack
     @Test
-    public void GivenTrackWithSameIdShouldUpdatedTrackOfThatId(){
+    public void givenTrackIdShouldReturnTheTrackDetails() {
+        trackRepository.save(track);
+        Track fetchTrack = trackRepository.findById(track.getTrackId()).get();
+        Assert.assertEquals(10, fetchTrack.getTrackId());
+    }
+
+    @Test
+    public void givenTrackIdShouldReturnSaveTrackFailure() {
+        Track testTrack = new Track(189, "Wow", "Fav");
+        trackRepository.save(testTrack);
+        Track fetchTrack = trackRepository.findById(track.getTrackId()).get();
+        Assert.assertNotSame(testTrack, fetchTrack);
+    }
+
+
+    @Test
+    public void givenInputShouldReturnAllTracks() {
+        Track track1 = new Track(456, "Celebrate", "DailyMix");
+        Track track2 = new Track(783, "Champion", "DailyMix");
+        trackRepository.save(track1);
+        trackRepository.save(track2);
+        List<Track> testTracks = new ArrayList<>();
+        testTracks.add(track1);
+        testTracks.add(track2);
+        List<Track> outputList = trackRepository.findAll();
+        Assert.assertEquals(testTracks, outputList);
+    }
+
+
+    @Test
+    public void givenTrackIdShouldReturnDeletedTrack() {
+        trackRepository.save(track);
+        trackRepository.delete(track);
+        Optional<Track> expected = Optional.empty();
+        Assert.assertEquals(expected, trackRepository.findById(track.getTrackId()));
+    }
+
+
+    @Test
+    public void givenTrackIdShouldReturnUpdatedTrack() {
         trackRepository.save(track);
         Track updatedTrack = trackRepository.findById(trackToUpdate.getTrackId()).get();
         updatedTrack.setTrackName(trackToUpdate.getTrackName());
         updatedTrack.setComments(trackToUpdate.getComments());
-        Track expected=trackToUpdate;
-        Track foundTrack=trackRepository.save(trackToUpdate);
-
-        Assert.assertEquals(expected,foundTrack);
+        Track expected = trackToUpdate;
+        Track foundTrack = trackRepository.save(trackToUpdate);
+        Assert.assertEquals(expected, foundTrack);
     }
-
-
-
 }
 
 
